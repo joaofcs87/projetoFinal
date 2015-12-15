27ef40ae56725ee54b313221bf116fa0a4e00090
joaofcs87 <joaofcss@yahoo.com.br>
2015-12-15 17:35:00 -0300

    spinner feito

--- app/src/main/java/br/senac/pi/controlenota/CadastraTurmaActivity.java
+++ app/src/main/java/br/senac/pi/controlenota/CadastraTurmaActivity.java
@@ -1,12 +1,15 @@
 package br.senac.pi.controlenota;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,10 +21,10 @@ public class CadastraTurmaActivity extends AppCompatActivity {
     //joaoCod
     private SQLiteDatabase database;
     private InstituicaoDB instituicaoDB;
-    private Instituicao instituicao;
-    private Spinner spn1;
-    //private List<String> nomes = new ArrayList<String>();
-    //private String nome;
+    private Spinner spinnerInstituicao;
+    private String[] campos = {"instituicao"};
+    private String instituicaoSelecionada;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +33,34 @@ public class CadastraTurmaActivity extends AppCompatActivity {
 
         //joaoCod
         instituicaoDB = new InstituicaoDB(this);
-        instituicao = new Instituicao();
-        List<Instituicao> listaResultado = instituicaoDB.selectInstituicoes();
-        /*nomes.add("Liceu Piauiense");
-        nomes.add("Escola Municipal Machado de Assis");
-        nomes.add("Instituto Federal de Educação Tecnológica");
-        nomes.add("Unidade Escolar Angelim");
-        */
-        spn1 = (Spinner) findViewById(R.id.spinnerInstituicao);
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, listaResultado);
+        database = instituicaoDB.getReadableDatabase();
+        Cursor cursorInstituicao = database.query("instituicoes", campos, null, null, null, null, "instituicao");
+        cursorInstituicao.moveToFirst();
+
+        ArrayList<String> nomes = new ArrayList<String>();
+
+        while (!cursorInstituicao.isAfterLast()) {
+            nomes.add(cursorInstituicao.getString(cursorInstituicao.getColumnIndexOrThrow("instituicao")));
+            cursorInstituicao.moveToNext();
+        }
+
+        spinnerInstituicao = (Spinner) findViewById(R.id.spinnerInstituicao);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, nomes);
         ArrayAdapter<String> spinnerArrayAdapter = arrayAdapter;
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        spn1.setAdapter(spinnerArrayAdapter);
+        spinnerInstituicao.setAdapter(spinnerArrayAdapter);
+        //joaoCod
+        spinnerInstituicao.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int posicao, long l) {
+                instituicaoSelecionada = adapterView.getItemAtPosition(posicao).toString();
+                //Toast.makeText(getApplicationContext(), "Instituicao: " + instituicaoSelecionada, Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
+            }
+        });
 
     }
 }
